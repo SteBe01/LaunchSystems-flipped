099ce14c5af431baa0b02e7b6d82eab3a5586b4d
SteBe01 <stefano3.belletti@mail.polimi.it>
2024-10-09 13:07:46 +0200

    Fix GLOM_SENS

--- GLOM_SENS.m
+++ GLOM_SENS.m
@@ -24,12 +24,13 @@ eps2_vect = linspace(eps2*(1-factor), eps2*(1+factor), n_vect)';
 eps3_vect = linspace(eps3*(1-factor), eps3*(1+factor), n_vect)';
 
 m_stag_vect = zeros(3, 1, n_vect, n_vect, n_vect);
-m_tot_vect = m_stag_vect;
-m_prop_vect = m_tot_vect;
+m_tot_vect = zeros(1, 1, n_vect, n_vect, n_vect);
+m_prop_vect = m_stag_vect;
 
 for i1 = 1:length(eps1_vect)
     for i2 = 1:length(eps1_vect)
         for i3 = 1:length(eps1_vect)
+            e = [eps1_vect(i1) eps2_vect(i2) eps3_vect(i3)]';
             [m_stag_temp, m_tot_temp, m_prop_temp] = TANDEM(Is, e, dv, m_pay, 0);
             m_stag_vect(:,:,i1,i2,i3) = m_stag_temp;
             m_tot_vect(:,:,i1,i2,i3) = m_tot_temp;
